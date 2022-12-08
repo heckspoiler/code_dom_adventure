d1083849742bf11f9face3dd209648125df89099
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2022-12-08 17:00:07 +0100

    more js

--- src/app/core/splash-screen.js
+++ src/app/core/splash-screen.js
@@ -12,12 +12,14 @@ class SplashScreen {
 
     document.head.appendChild(styleElement);
 
+    this.#splashElement.classList.add("splash-screen");
+
     const rootElement = document.getElementById("root");
+
     rootElement.appendChild(this.#splashElement);
     /* when it's private (#), it's not accessible from the outside. 
     But within the class we can use it and that's what we're going to do later in the destroy function. 
     So we can still use it inside the class, but not outside. This helps keeping the code less buggy. */
-    splashElement.classList.add("splash-screen");
   }
 
   destroy() {}
